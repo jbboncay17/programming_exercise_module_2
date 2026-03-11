bcc1024e476710b1f20fea63a853eaed80a7c254
jbboncay17 <jbboncay1@gmail.com>
2026-03-11 17:44:51 +0800

    final

--- even_numbers_counter.py
+++ even_numbers_counter.py
@@ -1,8 +1,11 @@
+bold='\033[1m'
+end='\033[0m'
+
 count=0
 
 for i in range(10):
-    num=int(input("Enter a number:"))
-
+    num=int(input(bold+"Enter a number:"+end))
     if num % 2 == 0:
         count=count+1
-print("Even Numbers Counter=", count)
+print("")
+print(bold+"Even Numbers Counter="+end, count)
