386b7be7fcc798d9359c902e80140bb1d747dda5
jbboncay17 <jbboncay1@gmail.com>
2026-03-11 16:13:17 +0800

    final

--- first_minus_all_remaining.py
+++ first_minus_all_remaining.py
@@ -1,8 +1,11 @@
-first = int(input("Enter number 1: "))
+bold='\033[1m'
+end='\033[0m'
+
+first = int(input(bold+"Enter number 1: "+end))
 result = first
 
 for i in range(9):
-    num = int(input("Enter number: "))
+    num = int(input(bold+"Enter number: "+end))
     result = result - num
-
-print("Result=", result)
+print("")
+print(bold+"Result="+end, result)
