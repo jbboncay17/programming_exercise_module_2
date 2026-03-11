4df1b06b7c4cd95b3d06081a196ed4344a2530c6
jbboncay17 <jbboncay1@gmail.com>
2026-03-11 12:31:12 +0800

    final

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,10 +1,10 @@
 bold="\033[1m"
 end="\033[0m"
 
-num1=int(input(bold+"Enter the first number:"+end))
-num2=int(input(bold+"Enter the second number:"+end))
-
+num1=float(input(bold+"Enter the first number:"+end))
+num2=float(input(bold+"Enter the second number:"+end))
+print("")
 if num1 > num2:
-    print(bold+"The smaller number is:"+end, num1)
-else:
     print(bold+"The smaller number is:"+end, num2)
+else:
+    print(bold+"The smaller number is:"+end, num1)
