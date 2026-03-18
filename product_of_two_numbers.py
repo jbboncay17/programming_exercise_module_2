b06c2dbe3a77ccfd75b1bf93f5a6aeb17f9e57ec
jbboncay17 <jbboncay1@gmail.com>
2026-03-18 21:40:51 +0800

    added a missing word

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -2,4 +2,4 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 product=num1*num2
-print("The product of two numbers is: ", product)
+print("The product of the two numbers is: ", product)
