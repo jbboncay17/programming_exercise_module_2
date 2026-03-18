f6cfa35631680c051db79b333192285877c4566e
jbboncay17 <jbboncay1@gmail.com>
2026-03-18 22:33:42 +0800

    last modification

--- how_many_odd_numbers.py
+++ how_many_odd_numbers.py
@@ -1,8 +1,12 @@
+bold='\033[1m'
+end='\033[0m'
+green='\033[32m'
+
 count=0
 
-for i in range(10):
-    odd=int(input(f"Enter number {i+1}: "))
+for i in range(5):
+    odd=int(input(bold+f"Enter number {i+1}: "+end))
     if odd % 2 != 0:
         count += 1
-
-print("There are",count, " odd numbers.")
+print("")
+print(bold+green+"There are"+end,count, bold+green+"odd numbers."+end)
